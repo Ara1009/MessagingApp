7f79df8ad94b6b633ad3b232e6f66fc46dbeb486
Aravind Subramanian <aravind@AravindsMacBookPro.local>
2017-11-11 16:13:37 -0500

    Initial view to login/signup

--- Messaging/LoginVC.swift
+++ Messaging/LoginVC.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import Firebase
+import SwiftKeychainWrapper
 
-class ViewController: UIViewController {
-
+class LoginVC: UIViewController {
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    var userUid: String!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func SignIn (_ sender: AnyObject) {
+    
+        if let email = emailField.text, let password = passwordfield.text{
+            
+            FIRAuth.auth()?.signIn(with: email,password: password, completion:
+                {(user, error) in
+                    
+                    if error == nil {
+                        
+                        self.userUid = user?.userUid
+                        
+                        KeychainWrapper.standard.set(self.userUid, forKey: "uid")
+                        
+                        performSegue(withIdentifier: "toMessages", sender: nil)
+                    
+                    } else {
+                        
+                        performSegue(withIdentifier:"toSignUp", sender: nil)
+                    }
+                    
+            })
+        }
     }
 
-
 }
-
